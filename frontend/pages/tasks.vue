0f204f0b96e228a6c325e280d183d949d6d5b781
shokm <26667526+shokm@users.noreply.github.com>
2023-02-09 23:35:48 +0900

    frontend 課題一覧画面実装(WIP)

--- frontend/pages/tasks.vue
+++ frontend/pages/tasks.vue
@@ -1,10 +1,44 @@
 <template>
-  <div>
-    <div v-for="task in tasks.tasks" :key="task.task_id">
-      <p>
-        {{ task.task_id }} {{ task.task_name }}
-        <nuxt-link :to="'/task/' + task.task_id">編集</nuxt-link>
-      </p>
+  <div class="flex justify-center">
+    <div class="m-4 w-full">
+      <div class="flex mt-8">
+        <h1 class="font-medium text-3xl">課題一覧</h1>
+        <nuxt-link
+          to="/subjects/tasks/"
+          class="ml-4 mt-1 font-medium text-2xl text-gray-400"
+          >教科ごと</nuxt-link
+        >
+      </div>
+      <h2 class="mt-8 font-medium text-xl">今日まで</h2>
+      <div v-for="task in tasks.tasks" :key="task.task_id">
+        <div
+          :to="'/task/' + task.task_id"
+          class="flex items-center mt-3 bg-white border border-DEFAULT shadow-lg rounded-xl"
+        >
+          <button
+            onclick="alert('完了ボタン押下')"
+            class="flex items-center justify-center m-3 mr-2 w-12 h-12 bg-gray-600 text-white shadow-lg rounded-lg"
+          >
+            ☑︎
+          </button>
+          <nuxt-link :to="'/task/' + task.task_id" class="p-3 pl-2 leading-6">
+            <p>{{ task.task_id }} {{ task.task_name }}</p>
+            <p>
+              教科：{{ task.subject_name }} 期限：
+              <span
+                v-if="
+                  !$dayjs($dayjs(task.deadline_at).format('YYYY-MM-DD')).isSame(
+                    $dayjs('1-01-01')
+                  )
+                "
+              >
+                {{ $dayjs(task.deadline_at).format('YYYY/MM/DD hh:mm') }}
+              </span>
+              <span v-else>---/--/--</span>
+            </p>
+          </nuxt-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
